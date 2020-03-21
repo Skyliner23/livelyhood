59ce3af1393d8913a9f3396b0da8ba26100e349c
Felix <felix.wanie@web.de>
2020-03-21 21:58:39 +0100

    Add core module with header to avoid code duplication

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    IonicModule.forRoot(),
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class CoreModule { }
