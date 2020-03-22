3951be96024233c750fe1adcabc7a2382d8bc4d7
Felix <felix.wanie@web.de>
2020-03-22 10:44:46 +0100

    added basic model for vendor

--- src/app/models/vendor.ts
+++ src/app/models/vendor.ts
@@ -1,5 +1,46 @@
 import { IVendor } from './vendor.interface';
 
-export class Vendor implements IVendor {
-  constructor(public name: string) {}
+export interface Vendor extends IVendor {
+    business: VendorBusiness;
+    contactInfo: VendorContactInfo;
+    products: VendorProduct[];
+    services: VendorService[];
+}
+
+export interface VendorBusiness {
+    businessName: string;
+    businessDescription: string;
+    businessOwner: string;
+    openingHours: string;
+    profilePic: string; 
+    businessRange: string[]; // add a range of zipCodes
+    branch: string[];
+}
+
+export interface VendorContactInfo {
+    street: string;
+    houseNumber: string;
+    zipCode: string;
+    telefone: string;
+    whatsApp: string;
+    email: string;
+    website: string;
+    socialMedia?: VendorSocialMedia;
+
+}
+
+export interface VendorSocialMedia {
+    instagram: string;
+    facebook: string;
+    twitter: string;
+    ebay: string;
+    other: [];
+}
+
+export interface VendorProduct {
+    name: string;
+}
+
+export interface VendorService {
+    name: string;
 }
