68428dbc09a64539164b465a070034c29745b6ed
elank <elank@gmx.de>
2020-03-22 15:32:03 +0100

    some minor changes to profiel page

--- src/app/pages/business-profile/business-profile.page.ts
+++ src/app/pages/business-profile/business-profile.page.ts
@@ -10,27 +10,44 @@ import { Vendor, VendorBusiness, VendorProduct, VendorContactInfo } from '../../
 })
 export class BusinessProfilePage implements OnInit {
 
-  vendor = {};
+  vendor: Vendor;
   id: string;
+  // TODO: change to false default
+  hasProfilePic = true;
 
   constructor(private activatedRoute: ActivatedRoute, private vendorService: VendorService) { }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe(params => {
       if (!params.has('businessId')) {
-        // redirect back to search
+        // redirect back to search if parameter is missing
         return;
       }
       const businessId = params.get('businessId');
       // TODO: already sanitized for db calls?
 
-      // TODO remove...
+      // TODO: remove...
       this.vendor = this.createFakeVendor(businessId)
 
       // call service with specific id
-      // this.vendorService.getVendorById(businessId).then(data => (this.vendor = data));
+      this.vendorService.getVendorById(businessId).then(data => {
+        // TODO: define criteria and default case
+        if(data && data.business){
+          this.vendor = data;
+          if (data.business && data.business.profilePic){
+            this.hasProfilePic = true;
+          }
+        }
+      }).catch(error => {
+        // TODO: define errorcase
+        console.log(error);
+      });
     });
   }
+
+
+
+  // -------------------- fake stuff ----------------------
 
   createFakeVendor(businessId: string): Vendor {
     let v = new DummyVendor;
@@ -49,8 +66,6 @@ export class BusinessProfilePage implements OnInit {
   }
 
 }
-
-// -------------------- fake stuff ----------------------
 
 export class DummyVendor implements Vendor {
   business: import("../../models/vendor").VendorBusiness;
